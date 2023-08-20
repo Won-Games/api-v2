c168c819b9a2697dd52dd8253a1dca32674979f0
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2023-08-20 18:45:52 -0300

    Create Populate Route and Controller

--- src/api/game/controllers/game.ts
+++ src/api/game/controllers/game.ts
@@ -2,6 +2,15 @@
  * game controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::game.game');
+export default factories.createCoreController(
+  "api::game.game",
+  ({ strapi }) => ({
+    async populate(ctx) {
+      console.log("RODANDO NO SERVIDOR");
+
+      ctx.send("FINALIZADO NO CLIENT");
+    },
+  })
+);
---
+++ src/api/game/routes/populate.ts
@@ -0,0 +1,9 @@
+export default {
+  routes: [
+    {
+      method: "POST",
+      path: "/games/populate",
+      handler: "game.populate",
+    },
+  ],
+};
