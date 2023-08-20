e478525872abb114c435ff3f90000f834510fb1c
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2023-08-20 16:13:52 -0300

    Add Custom Theme

---
+++ src/admin/app.tsx
@@ -0,0 +1,37 @@
+import Icon from "./extensions/icon.png";
+import Logo from "./extensions/logo.svg";
+
+export default {
+  config: {
+    auth: {
+      logo: Logo,
+    },
+    head: {
+      favicon: Icon,
+    },
+    locales: [],
+    translations: {
+      en: {
+        "Auth.form.welcome.title": "Welcome to Won Games!",
+        "Auth.form.welcome.subtitle": "Log in to your account",
+        "app.components.LeftMenu.navbrand.title": "Won Games Dashboard",
+      },
+    },
+    menu: {
+      logo: Icon,
+    },
+    theme: {
+      light: {},
+      dark: {
+        colors: {
+          primary100: "#030415",
+          primary600: "#f231a5",
+          primary700: "#f231a5",
+          neutral0: "#0d102f",
+          neutral100: "#030415",
+        },
+      },
+    },
+  },
+  bootstrap() {},
+};
